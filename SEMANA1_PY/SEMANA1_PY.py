0e18a009f98870390dfd0ba39e590ef955f1a51f
Gianfranco <gianfrancops10@gmail.com>
2025-09-05 08:48:38 -0500

    Subiendo ejer3 de sem3

--- SEMANA1_PY/SEMANA1_PY.py
+++ SEMANA1_PY/SEMANA1_PY.py
@@ -8,5 +8,17 @@ def ejer2():
     print("\"Gianfranco\"")
 
 def ejer3():
-    numero1= input("Ingrese el primer número")
-ejer2()
+    num1= int(input("Ingrese el primer número: "))
+    num2= int(input("Ingrese el segundo número: "))
+
+    suma=num1 + num2
+    resta=num1 - num2
+    multiplicacion=num1 * num2
+    division=num1 / num2
+
+    print("La suma es ", suma)
+    print("La resta es ", resta)
+    print("La multiplicación es ", multiplicacion)
+    print("La división es ", division)
+
+ejer3()
